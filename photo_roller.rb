52ba2c8a3da28748b94ba9a54de815445bc37bd5
mwalker <matt.r.walker@gmail.com>
2010-03-01 20:53:59 +0000

    Add exclusions -- still need support within Gallery::Remote for various file types
    git-svn-id: svn+ssh://mwalker.info/home/mwalker/svn/gallery_remote@79 ed2e9cbb-6540-0410-a968-88b1c1d47aaa

--- photo_roller.rb
+++ photo_roller.rb
@@ -20,6 +20,12 @@ class PhotoRoller
     end
   end
 
+  def self.reject_images(iphoto_images, msg, &block)
+    original_size = iphoto_images.size
+    iphoto_images.reject!(&block)
+    puts "#{original_size - iphoto_images.size} of #{original_size} photos #{msg}" if iphoto_images.size < original_size
+  end
+
   def upload(account)
     album_data = IPhoto::AlbumData.new(account[:album_data])
     puts "Loaded iPhoto album data (#{album_data.rolls.size} rolls, #{album_data.images.size} images)"
@@ -29,15 +35,25 @@ class PhotoRoller
       album_cache = albums
 
       parent_album = album_cache.find{ |a| a.title == account[:parent_album] }
-      #parent_album ||= Gallery::Album.new(remote, { 'name' => '0', 'title' => 'Gallery' })
+      raise "Could not find parent album '#{account[:parent_album]}; create it or modify account settings'" unless parent_album
 
+      # TODO: remove
       count, limit = 1, 50
       File.open(account[:rejects_file], 'w') do |rejects|
         album_data.rolls.each do |roll|
           iphoto_images = roll.images
+          # TODO: remove
           next if iphoto_images.size > 10
           break if count > limit
           count += 1
+
+          # Exclusions
+          PhotoRoller.reject_images(iphoto_images, 'excluded by media type'){ |iphoto_image| account[:excluded_media_types].include?(iphoto_image.media_type) }
+          PhotoRoller.reject_images(iphoto_images, 'excluded by image type'){ |iphoto_image| account[:excluded_image_types].include?(iphoto_image.image_type) }
+          PhotoRoller.reject_images(iphoto_images, 'excluded by keyword'){ |iphoto_image| !(account[:excluded_keywords] & iphoto_image.keywords).empty? }
+
+          # Don't create album if we've filtered all images
+          next if iphoto_images.empty? && !roll.images.empty?
 
           remote_album = album_cache.find{ |a| a.title == roll.name }
           if remote_album
@@ -45,17 +61,18 @@ class PhotoRoller
             puts "Album exists: #{roll.name}"
             remote_photos = remote_album.images.map(&:caption)
 
-            iphoto_images = iphoto_images.reject{ |iphoto_image| remote_photos.include?(iphoto_image.caption) }
-            puts "#{roll.images.size - iphoto_images.size} of #{roll.images.size} photos already exist in album" if iphoto_images.size < roll.images.size
+            PhotoRoller.reject_images(iphoto_images, 'already exist in album'){ |iphoto_image| remote_photos.include?(iphoto_image.caption) }
           else
             puts "Album missing: #{roll.name}"
             parent_album.add_album(roll.name)
 
             # TODO: get actual name, update albums, etc.
             album_cache = albums
-
             remote_album = album_cache.find{ |a| a.title == roll.name }
           end
+
+          # Don't bother uploading 0 images
+          next if iphoto_images.empty?
 
           puts "Uploading #{iphoto_images.size} photos"
           iphoto_images.each do |iphoto_image|
