9e8e5d34315308b58e77a27073f287fade12f691
mwalker <matt.r.walker@gmail.com>
2011-02-02 03:06:19 -0600

    Gross patch for ridiculous rbuf_fill behavior

--- roll_photos.rb
+++ roll_photos.rb
@@ -5,4 +5,15 @@ require 'photo_roller'
 
 STDOUT.sync = true
 
+# See: http://www.ruby-forum.com/topic/105212
+module Net
+  class BufferedIO
+    def rbuf_fill
+      timeout(@read_timeout, ProtocolError) {
+        @rbuf << @io.sysread(BUFSIZE)
+      }
+    end
+  end
+end
+
 PhotoRoller.new.upload(YAML.load_file('config/account.yml'))
